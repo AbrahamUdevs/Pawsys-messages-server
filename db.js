75f52bc6a72583874562c1b50918ebaa0882e710
AbrJPO98 <abrahamjosue293@gmail.com>
2024-12-13 18:11:06 -0600

    Update

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize('pawsy', 'root', '', {
-  host: 'localhost',
-  port: process.env.DBPORT,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   logging: false,
 });
